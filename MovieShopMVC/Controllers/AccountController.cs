27f9ed7e91f817b3476041ab303cddce87a31ba7
Shao Zheng <zhengshaoliang1112@gmail.com>
2022-01-19 15:17:35 -0500

    new code

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -14,7 +14,7 @@ namespace MovieShopMVC.Controllers
          * Register
          * Logout
          *--------------------------------------------------
-         */
+         */        
         //showing the register view
 
         private readonly IAccountService _accountService;
